4be557aae518d6d23b2f758c4dd2a3979950ce36
SkyBlyvion <richtofen60@gmail.com>
2024-10-11 08:54:37 +0200

    initial commit

---
+++ app/src/main/java/com/cfa/immo/ImmoApplication.java
@@ -0,0 +1,22 @@
+package com.cfa.immo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@SpringBootApplication
+public class ImmoApplication {
+
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String home(){
+		return "bonjour le CFA";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoApplication.class, args);
+	}
+
+}
